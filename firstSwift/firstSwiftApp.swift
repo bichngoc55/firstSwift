a0fece395bcf8da4f1570df83de1b45b68e5cdc8
Gấu <115764988+bichngoc55@users.noreply.github.com>
2025-02-05 16:58:57 +0700

    Initial Commit

---
+++ firstSwift/firstSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstSwiftApp.swift
+//  firstSwift
+//
+//  Created by Bích Ngọc on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct firstSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
